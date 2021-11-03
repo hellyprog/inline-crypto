8f9659bf5ead4a9873e5c5912ea4a39d8a7d2275
volodymyr.romaniv <v.b.romaniv@gmail.com>
2021-11-03 19:48:52 +0200

    Implemented object retrievement

--- extension/scripts/provider.js
+++ extension/scripts/provider.js
@@ -1,2 +1,25 @@
-var radios = document.querySelectorAll('input[type=radio][name="info_provider"]');
-radios.forEach(radio => radio.addEventListener('change', () => alert(radio.value)));
+const infoProviderKey = 'info-provider';
+
+document.addEventListener('DOMContentLoaded', function() {
+    getStorageValues();
+    InitRadioEventListeners();
+});
+
+function InitRadioEventListeners() {
+    let radios = document.querySelectorAll(`input[type=radio][name="${infoProviderKey}"]`);
+    radios.forEach(radio => radio.addEventListener('change', () => {
+        let storeObject = {};
+        storeObject[infoProviderKey] = radio.value;
+
+        chrome.storage.sync.set(storeObject, function(toggleOn) {
+            console.log(toggleOn);
+            console.log(`Value is set to ${radio.value}`);
+        });
+    }));
+}
+
+function getStorageValues() {
+    chrome.storage.sync.get(infoProviderKey, function(obj) {
+        console.log(obj[infoProviderKey]);
+    });
+}
