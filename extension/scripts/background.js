8a0c9765f5995061191adce7f6c12f32a2782412
volodymyr.romaniv <v.b.romaniv@gmail.com>
2021-11-09 00:02:06 +0200

    Added content script loading into pages

--- extension/scripts/background.js
+++ extension/scripts/background.js
@@ -1,12 +1,12 @@
-chrome.runtime.onInstalled.addListener(() => {
-    chrome.contextMenus.create({
-        id: 'inline-crypto-cm',
-        contexts: ['selection'],
-        title: 'Default',
-        visible: false
-    });
-});
+// chrome.runtime.onInstalled.addListener(() => {
+//     chrome.contextMenus.create({
+//         id: 'inline-crypto-cm',
+//         contexts: ['selection'],
+//         title: 'Default',
+//         visible: false
+//     });
+// });
 
-chrome.contextMenus.onClicked.addListener((info, tab) => {
-    console.log(info.selectionText);
-});
+// chrome.contextMenus.onClicked.addListener((info, tab) => {
+//     console.log(info.selectionText);
+// });
---
+++ extension/scripts/content-script.js
@@ -0,0 +1,3 @@
+fetch(chrome.runtime.getURL('/templates/info-popup.template.html')).then(r => r.text()).then(html => {
+    document.body.insertAdjacentHTML('beforeend', html);
+});
