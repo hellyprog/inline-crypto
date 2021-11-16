c7ef2c09d7ad9a27d487906bb5410b50ebd4509d
volodymyr.romaniv <v.b.romaniv@gmail.com>
2021-11-16 23:27:07 +0200

    Moved styles from script to injected css

--- extension/scripts/content-script.js
+++ extension/scripts/content-script.js
@@ -4,23 +4,13 @@ fetch(chrome.runtime.getURL('/templates/info-popup.template.html'))
     .then(r => r.text())
     .then(html => {
         document.body.insertAdjacentHTML('beforeend', html);
+        initPopup();
     });
-
-initPopup();
 
 //Declarations
 
 function initPopup() {
-    applyPopupStyles();
     fetchPopupData();
-}
-
-function applyPopupStyles() {
-    var infoPopup = document.querySelector('#info-popup');
-    infoPopup.style.position = 'absolute';
-    infoPopup.style.top = '50%';
-    infoPopup.style.left = '50%';
-    infoPopup.style.backgroundColor = 'orange';
 }
 
 function fetchPopupData() {
