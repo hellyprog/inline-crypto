0f932b9dfadcedf8d7cee7f2a8c0b84039453def
volodymyr.romaniv <v.b.romaniv@gmail.com>
2021-12-05 11:28:23 +0200

    Added contract copy to clipboard

--- extension/info-popup-component/info-popup.js
+++ extension/info-popup-component/info-popup.js
@@ -1,3 +1,13 @@
+function copyContract(e) {
+    debugger;
+    var data = [new ClipboardItem({ "text/plain": new Blob([e.innerText], { type: "text/plain" }) })];
+    navigator.clipboard.write(data).then(function() {
+        console.log("Copied to clipboard successfully!");
+    }, function() {
+        console.error("Unable to write to clipboard. :-(");
+    });
+}
+
 function popupBackgroundClick() {
     let infoPopup = document.getElementById("info-popup-background");
     infoPopup.remove();
