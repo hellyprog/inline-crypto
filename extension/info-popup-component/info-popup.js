d7556ffe3f22c52a5ba66bf0dfbde33313060205
volodymyr.romaniv <v.b.romaniv@gmail.com>
2021-12-07 23:35:47 +0200

    Changed popup closing element

--- extension/info-popup-component/info-popup.js
+++ extension/info-popup-component/info-popup.js
@@ -1,5 +1,4 @@
 function copyContract(e) {
-    e.stopPropagation();
     var data = [new ClipboardItem({ "text/plain": new Blob([e.target.innerText], { type: "text/plain" }) })];
     navigator.clipboard.write(data).then(function() {
         console.log("Copied to clipboard successfully!");
